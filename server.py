464e1290f2a61d1c899597f47626ea09fb525714
jd-lo <jdlo@ualberta.ca>
2023-03-09 17:02:00 -0700

    Server seems to carry out all basic tasks (correctly)

--- server.py
+++ server.py
@@ -22,7 +22,7 @@
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, Response
 import json
 app = Flask(__name__)
 app.debug = True
@@ -57,7 +57,11 @@ class World:
 # you can test your webservice from the commandline
 # curl -v   -H "Content-Type: application/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}' 
 
-myWorld = World()          
+myWorld = World()     
+mime = {
+    "json": "application/json",
+    "html": "text/html",
+} 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
@@ -74,27 +78,45 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return flask.redirect('/static/index.html', code=301)
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    body = flask_post_json()
+
+    #Entity = client (i.e person drawing), key = coordinate-pair, value = coordinates (dict) ?
+    for key, value in body.items():
+        myWorld.update(entity, key, value)
+
+    updated_entity = myWorld.get(entity)
+
+    res = Response(response=updated_entity, status=200, mimetype=mime["json"])
+    return res
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    world = json.dumps(myWorld.world())
+
+    res = Response(response=world, status=200, mimetype=mime["json"])
+    return res
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    entity = json.dumps(myWorld.get(entity))
+    #Returns an empty dict if no entity (client will have to decide what to do with this)
+    return Response(response=entity, status=200)
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    world = json.dumps(myWorld.world())
+
+    res = Response(response=world, status=200, mimetype=mime["json"])
+    return res
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
